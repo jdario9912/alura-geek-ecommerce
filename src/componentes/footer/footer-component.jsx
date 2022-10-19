948fcf19ad5d070590c93d4e7823c8b49320e106
Joel Dario <joeldario9912@gmail.com>
2022-10-18 23:23:26 -0300

    finalice el footer

--- src/componentes/footer/footer-component.jsx
+++ src/componentes/footer/footer-component.jsx
@@ -4,37 +4,39 @@ import Logo from '../logo';
 import Vinculo from '../vinculo';
 import { VinculoModel } from '../../modelos/vinculo-model';
 
-
 const FooterComponent = () => {
   const sizeLogoFooter = new LogoSize('', '');
-  // const vQS = new VinculoModel('Quienes Somos', '#', '');
+
+  const lineHeight = 'leading-7'
 
   return (
-    <footer className='flex bg-[#EAF2FD]'>
-      <div>
-        <Logo sizes={sizeLogoFooter} />
-        <div>
-          <Vinculo props={new VinculoModel('Quienes Somos', '#', '')}/>
-          <Vinculo props={new VinculoModel('Programa de Fidelidad', '#', '')}/>
-          <Vinculo props={new VinculoModel('Nuestras Tiendas', '#', '')}/>
-          <Vinculo props={new VinculoModel('Quiero una Franquicia', '#', '')}/>
-          <Vinculo props={new VinculoModel('Politica de Privacidad', '#', '')}/>
-          <Vinculo props={new VinculoModel('Anúncie Aquí', '#', '')}/>
-        </div>
-        <form action="">
-          <label htmlFor="nombre">
-            Nombre
-            <input type="text" id='nombre' />
+    <footer className='flex flex-col items-center bg-[#EAF2FD] pt-4 px-4'>
+      <Logo sizes={sizeLogoFooter} />
+      <div className='flex flex-col items-center my-4'>
+          <Vinculo props={ new VinculoModel('Quienes somos', '#', lineHeight)} />
+          <Vinculo props={ new VinculoModel('Política de privacidad', '#', lineHeight)} />
+          <Vinculo props={ new VinculoModel('Programa de fidelidad', '#', lineHeight)} />
+          <Vinculo props={ new VinculoModel('Nuestras tiendas', '#', lineHeight)} />
+          <Vinculo props={ new VinculoModel('Quiero una franquicia', '#', lineHeight)} />
+          <Vinculo props={ new VinculoModel('Anúncie aquí', '#', lineHeight)} />
+      </div>
+      <div className='my-4'>
+        <h2 className='font-bold'>Hable con nosotros</h2>
+        <form action="" className=''>
+          <label htmlFor="nombre" className='flex flex-col bg-[#FFFFFF] border-b-2 border-slate-300 mt-4'>
+            Nombre:
+            <input type="text" id='nombre' className='w-full' />
           </label>
-          <label htmlFor="mensaje">
-            Mensaje
-            <textarea name="mensaje" id="mensaje" cols="30" rows="10"></textarea>
+          <label htmlFor="mensaje" className='flex flex-col bg-[#FFFFFF] border-b-2 border-slate-300 mt-4'>
+            Mensaje:
+            <textarea name="mensaje" id="mensaje" cols="30" rows="5" className='w-full'></textarea>
           </label>
-          <button type="submit">Enviar Mensaje</button>
+          <button type="submit" className='py-2 px-6 bg-blue-600 mt-2 text-[#FFFFFF]'>Enviar Mensaje</button>
         </form>
       </div>
-      <div>
-
+      <div className='bg-[#FFFFFF] w-screen'>
+        <p className='text-center'>Desarrolado por Joel</p>
+        <p className='text-center'>2022</p>
       </div>
     </footer>
   );
