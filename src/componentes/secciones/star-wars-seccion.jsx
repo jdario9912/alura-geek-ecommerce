dd595a491801b252972fe93511318ad128de0b9b
Joel Dario <joeldario9912@gmail.com>
2022-10-20 16:59:06 -0300

    subi la api a la nube y me conecte

--- src/componentes/secciones/star-wars-seccion.jsx
+++ src/componentes/secciones/star-wars-seccion.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import EncabezadoSeccion from '../encabezado-seccion';
 import Producto from '../producto';
 
 const StarWarsSeccion = () => {
+  useEffect(
+    () => {
+      fetch('https://db-alura-geek-ecommerce.herokuapp.com/consolas')
+            .then(response => response.json())
+            .then((info) => console.log(info[0].nombre))
+          }
+  );
+  
+
   return (
     <section className='p-4'>
       <EncabezadoSeccion titulo='StarWars' />
       <div className='flex flex-wrap gap-4'>
         <Producto 
-          nombre='Algun nombre' 
+          nombre='algun nombre'
           precio={100} 
           vinculoVerMas='/producto-descripcion'
           url='https://github.com/jdario9912/e-commers/blob/gh-pages/assets/img/todos/productos/epRFE_hBNjo.png?raw=true'
