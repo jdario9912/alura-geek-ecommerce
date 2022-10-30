2b3f722dcc799b0ba8cce7d975e3303b571761cf
Joel Dario <joeldario9912@gmail.com>
2022-10-30 00:36:34 -0300

    enlace las rutas con las paginas

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from './paginas/login';
 import ProductosTodos from './paginas/productos-todos';
 import Error404 from './paginas/error404';
+import AgregarProducto from './paginas/agregar-producto';
 
 function App() {
   const [showFormSearch, setShowFormSearch] = useState(false);
@@ -27,12 +28,15 @@ function App() {
   return (
     <div className="App text-slate-700">
       <Header accionLupa={ controlShowFormSearch } showFormSearch={ showFormSearch }/>
-      <Routes>
-        <Route path="*" element={ <Error404 /> } />
-        <Route path="/" element={ <Home /> } />
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/productos-todos" element={ <ProductosTodos /> } />
-      </Routes>
+      <div className='bg-neutral-100'>
+        <Routes>
+          <Route path="*" element={ <Error404 /> } />
+          <Route path="/" element={ <Home /> } />
+          <Route path="/login" element={ <Login /> } />
+          <Route path="/productos-todos" element={ <ProductosTodos /> } />
+          <Route path="/agregar-producto" element={ <AgregarProducto /> } />
+        </Routes>
+      </div>
       <Overlay visible={ showFormSearch } accionOverlay={ controlShowFormSearch } />
       <FooterComponent />
     </div>
--- src/paginas/productos-todos.jsx
+++ src/paginas/productos-todos.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Producto from '../componentes/producto';
+import Vinculo from '../componentes/vinculo';
+import { VinculoModel } from '../modelos/vinculo-model';
 
 const ProductosTodos = () => {
+
+  const vinculoEstilos = 'bg-blue-600 text-slate-200 p-2';
   return (
     <div className='p-4'>
-      <h3 className='text-xl font-medium mb-4'>Todos los productos</h3>
-      <Producto nombre={'tazon'} precio={35627} vinculoVerMas={'#'}/>
+      <div className=''>
+        <h3 className='text-xl font-medium mb-4'>Todos los productos</h3>
+        <Vinculo props={ new VinculoModel('Agregar producto', '/agregar-producto', vinculoEstilos)} />
+      </div>
+      <div>
+        <Producto nombre={'tazon'} precio={35627} vinculoVerMas={'#'}/>
+      </div>
     </div>
   );
 }
